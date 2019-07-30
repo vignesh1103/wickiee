2e82db01218f318655748954cf4314f3f7c0bd8d
vignesh1103 <53211129+vignesh1103@users.noreply.github.com>
2019-07-31 01:11:47 +0530

    Add files via upload

---
+++ vowelorconsonant.py
@@ -0,0 +1,12 @@
+char=input("")
+if char.isalpha():
+  if char=='a'or char=='e' or char=='i' or char=='o' or char=='u':
+    print("Vowel")
+  elif char=='A' or char=='E' or char=='I' or char=='O' or char=='U':
+    print("Vowel")
+  else:
+    print("Consonant")
+else:
+  print("invalid")
+
+  
